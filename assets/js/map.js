c0af215c92674e6d8a7d8105f83f1a444fcb5415
adamparker75 <adamparker75@sky.com>
2020-07-09 19:13:39 +0000

    Aded script tags for google maps and js to initialise map

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: {
+            lat: 54.7753,
+            lng: 1.5849
+        }
+    });
+}
