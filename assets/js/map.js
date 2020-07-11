e13f44f704339f79acc5ed3de6534fe8da2faa37
adamparker75 <adamparker75@sky.com>
2020-07-11 19:40:56 +0000

    Removed marker clusters and changed to markers with info window

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,29 +1,33 @@
-//Code taken from Google Maps lesson in Code Institute Interactive Front End Module//
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 10,
-        center: { lat: 54.7817348, lng: -1.6222762}
+//Code taken from Stac Overflow, https://stackoverflow.com/questions/30012913/google-map-api-v3-add-multiple-infowindows //
+
+  var locations = [
+    { lat: 54.8849314, lng: -1.6673812, name: "Beamish Museum" },
+    { lat: 54.5420335, lng: -1.9175423, name: "Bowes Museum" },
+    { lat: 54.7732329, lng: -1.5785808, name: "Durham Cathedral" },
+    { lat: 54.6506813, lng: -2.1889541, name: "High Force Waterfall" },
+    { lat: 54.8014524, lng: -1.6782121, name: "Diggerland Durham" },
+    { lat: 54.5915822, lng: -1.8041618, name: "Raby Castle" },
+  ];
+  
+  function initMap() {
+     var infowindow = new google.maps.InfoWindow(); 
+      var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 10,
+    center: { lat: 54.7817348, lng: -1.6222762 },
+  });
+
+    function placeMarker( loc ) {
+    const marker = new google.maps.Marker({
+      position : new google.maps.LatLng( loc.lat, loc.lng ),
+      map : map
     });
-
-
-var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var locations = [
-    { lat: 54.8849314, lng: -1.6673812 },
-    { lat: 54.5420335, lng: -1.9175423 },
-    { lat: 54.7732329, lng: -1.5785808 },
-    { lat: 54.6506813, lng: -2.1889541 },
-    { lat: 54.8014524, lng: -1.6782121 },
-    { lat: 54.5915822, lng: -1.8041618 }
-];
-
-   var markers = locations.map(function (location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
+    google.maps.event.addListener(marker, 'click', function(){
+        infowindow.close(); // Close previously opened infowindow
+        infowindow.setContent(`<div id="infowindow">${loc.name}</div>`);
+        infowindow.open(map, marker);
     });
+  }
 
-        var markerCluster = new MarkerClusterer(map, markers,
-        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-};
+  locations.forEach( placeMarker );
+
+}
