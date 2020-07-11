ab6042af80bfed64a963f51c19a1212c73773361
adamparker75 <adamparker75@sky.com>
2020-07-11 18:56:21 +0000

    Added code in maps.js to put markers for 6 tourist attractions on the map

--- assets/js/map.js
+++ assets/js/map.js
@@ -4,4 +4,26 @@ function initMap() {
         zoom: 10,
         center: { lat: 54.7817348, lng: -1.6222762}
     });
-}
+
+
+var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var locations = [
+    { lat: 54.8849314, lng: -1.6673812 },
+    { lat: 54.5420335, lng: -1.9175423 },
+    { lat: 54.7732329, lng: -1.5785808 },
+    { lat: 54.6506813, lng: -2.1889541 },
+    { lat: 54.8014524, lng: -1.6782121 },
+    { lat: 54.5915822, lng: -1.8041618 }
+];
+
+   var markers = locations.map(function (location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+        var markerCluster = new MarkerClusterer(map, markers,
+        { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+};
