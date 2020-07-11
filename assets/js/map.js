35dced1eebf3f18f1a9c617dddf6815fc9acf94f
adamparker75 <adamparker75@sky.com>
2020-07-11 10:31:56 +0000

    Added more zoom on google map, removed link to Durham Tourism and centered social links on both pages

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,10 +1,7 @@
 //Code taken from Google Maps lesson in Code Institute Interactive Front End Module//
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 7,
-        center: {
-            lat: 54.7753,
-            lng: 1.5849
-        }
+        zoom: 10,
+        center: { lat: 54.7817348, lng: -1.6222762}
     });
 }
