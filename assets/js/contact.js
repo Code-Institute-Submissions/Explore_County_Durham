8ee5c49e3621e6de0ec4bf667ae8fd2d79bec880
adamparker75 <adamparker75@sky.com>
2020-07-27 19:27:56 +0000

    Amended contact.js to alert when submitted

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,16 +1,16 @@
 function sendMail(contactForm) {
     emailjs.send("gmail", "durham", {
-        "from_name": contactForm.name.value, //This is the name that we gave for the name input field in the contact.html form //
-        "from_email": contactForm.email.value, //This is the name that we gave for the email input field in the contact.html form 
-        "website_feature_request": contactForm.comment.value //This is the name that we gave for the text area field in the contact.html form 
+        "from_name": contactForm.name.value, 
+        "from_email": contactForm.email.value,
+        "website_feature_request": contactForm.comment.value 
     })
         .then (
         function(response) {
-            console.log("SUCCESS", response); //If sending an email has been a success we consoole.log the word success.
+            alert("Your request has been sent.", response); 
         },
        function(error) {
-           console.log("FAILED", error)
+           alert("FAILED", error)
        }
        );
-       return false; //To block from loading a new page
+       return false; 
 }
