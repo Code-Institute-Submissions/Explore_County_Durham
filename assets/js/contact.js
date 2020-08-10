7fc4507a8a8a6a569a36e37a3f46e6d657d69f55
adamparker75 <adamparker75@sky.com>
2020-08-10 18:13:21 +0000

    Changes to contact.js after mentor session, tidied up code

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,18 +1,32 @@
-// This codes used from the Code Institute Interactove Front End Module
+// This code is taken from the Code Institute Interactive Front End Module
+
+function handleSuccess(response) {
+    alert("Your request has been sent.", response);
+    // Disable display of the overlay
+}
+
+function handleFailure(error) {
+    alert("Couldn't send email at this time, please try again later.");
+    // Disable display of the overlay
+}
 
 function sendMail(contactForm) {
     emailjs.send("gmail", "durham", {
         "from_name": contactForm.name.value, 
         "from_email": contactForm.email.value,
         "website_feature_request": contactForm.comment.value 
-    })
-        .then (
-        function(response) {
-            alert("Your request has been sent.", response); 
-        },
-       function(error) {
-           alert("FAILED", error)
-       }
+    }).then (
+           handleSuccess,
+           handleFailure,
        );
-       return false; 
 }
+
+$('#contactForm').on('submit', function(e) {
+    // Enable display of the overlay
+    // Select the button and disable it, display spinner
+    e.preventDefault();
+    sendMail(this);
+    $(this).each(function() {
+         this.reset();
+    });
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#learn-more-btn").on("click", () => {
+    $("#learn-more-btn").on("click", function(){
         $(".page-header").toggle("slow");
         $("#learn-more-btn").toggle("slow");
         $(".site-info").fadeToggle("slow");
